cc6df51802ade3dac432b7ebd9b67fe387822d18
GaneshKhadsan <ganeshskhadsan@gmail.com>
2019-08-01 17:19:42 +0530

    second commit

--- Hello.java
+++ Hello.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 public class Hello {
   public static void main(String[] args) 
   {
-    Scanner input = new Scanner (System.in);
-    System.out.print("Input your first name: ");
-    String fname = input.next();
-    System.out.print("Input your last name: ");
-    String lname = input.next();
-    System.out.println();
-    System.out.println("Hello \n"+fname+" "+lname);
+    System.out.println("Hello World");
   }
 }
